703870fcf6894cb9267354a9167d56c0d0be0433
12Emma <127460982+12Emma@users.noreply.github.com>
2024-06-05 07:41:27 +0100

    Update tour_hanoi.ml

--- tour_hanoi.ml
+++ tour_hanoi.ml
@@ -1,35 +1,36 @@
 let hanoi n =
   let rec hanoi_aux src aux dest n =
-    if n = 1 then  
-      Printf.printf "Déplacer un disque de %s à %s\n" src dest
-    else begin  
+    if n = 1 then
+      Printf.printf "%s - %s\n" src dest
+    else begin
       hanoi_aux src dest aux (n - 1);
-      Printf.printf "Déplacer un disque de %s à %s\n" src dest;
+      Printf.printf "%s - %s\n" src dest;
       hanoi_aux aux src dest (n - 1)
     end
   in
-  let debut = Sys.time () in 
-  hanoi_aux "A" "B" "C" n; 
+  let debut = Sys.time () in
+  hanoi_aux "A" "B" "C" n;
   let fin = Sys.time () in
-  Printf.printf "Temps d'exécution pour n=%d : %f secondes\n" n (fin -. debut) 
-
-let n = 5¹
+  Printf.printf "Temps d'exécution pour n=%d : %f secondes\n" n (fin -. debut)
+  
 let rec temps_ecouler n =
-  let debut = Sys.time () in 
-  hanoi n;  
-  let fin = Sys.time () in  
-  if (fin -. debut) > 10.0 then 
-    n 
+  let debut = Sys.time () in
+  hanoi n;
+  let fin = Sys.time () in
+  if (fin -. debut) > 10.0 then
+    n
   else
-    temps_ecouler (n + 1) 
-
+    temps_ecouler (n + 1)
 let () =
-  let seuil = temps_ecouler 1 in 
-  Printf.printf "À partir de n=%d, on dépasse 10 secondes\n" seuil 
+  let seuil = temps_ecouler 1 in
+  Printf.printf "À partir de n=%d, on dépasse 10 secondes\n" seuil
 
-  let () =
-  let n = 3 in 
+let test_hanoi n =
   Printf.printf "Appuyez sur Entrée pour commencer...\n";
   ignore (read_line ());
-  stop := false;
+  hanoi n
+
+let () =
+  let n = 3 in
   test_hanoi n
+
